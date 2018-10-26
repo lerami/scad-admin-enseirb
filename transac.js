0c47e09ae53f06f3a1bf5e1bb54769d0be511d44
elise <elise.marie.lau@gmail.com>
2018-10-26 17:04:11 +0200

    add mongoose

--- transac.js
+++ transac.js
@@ -1,55 +1,56 @@
-const bodyParser= require('body-parser')
+const bodyParser = require('body-parser')
 var fs = require('fs');
 var express = require("express")
+const mongoose = require('mongoose');
+const URI = "mongodb://teamscad:!teamscad2018@scad-shard-00-00-nayx1.mongodb.net:27017,scad-shard-00-01-nayx1.mongodb.net:27017,scad-shard-00-02-nayx1.mongodb.net:27017/test?replicaSet=scad-shard-0&ssl=true";
+
 var app = express()
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.post("/transactions", function(req, res) {
-  const transactions_exemple = {
-    action: req.body.action,
-    name: req.body.name
-  }
-  console.log("test")
-  console.log(transactions_exemple)
-  if( req.body.action=="Ajout"){
-    var read_string=fs.readFile('test.txt','utf-8',function(err,data){
-      if(err){
+mongoose.connect(URI, { useMongoClient: true });
+
+app.post("/transactions", function (req, res) {
+
+  if (req.body.action == "Ajout") {
+    var read_string = fs.readFile('test.txt', 'utf-8', function (err, data) {
+      if (err) {
         return console.error(err);
       }
       console.log(data);
-    fs.writeFile('test.txt',data+"\n [AJOUT] "+req.body.name,function(err,data){
-      if(err){
-        return console.error(err);
-      }
-      console.log('file is written asynchronously');
+      fs.writeFile('test.txt', data + "\n [AJOUT] " + req.body.name, function (err, data) {
+        if (err) {
+          return console.error(err);
+        }
+        console.log('file is written asynchronously');
+      })
     })
-  })
-  res.send("Sucess")
+    res.send("Sucess")
   }
-  else if (req.body.action=="Delete"){
-    var read_string=fs.readFile('test.txt','utf-8',function(err,data){
-      if(err){
+  else if (req.body.action == "Delete") {
+    var read_string = fs.readFile('test.txt', 'utf-8', function (err, data) {
+      if (err) {
         return console.error(err);
       }
       console.log(data);
-    fs.writeFile('test.txt',data+"\n [DELETE] "+req.body.name,function(err,data){
-      if(err){
-        return console.error(err);
-      }
-      console.log('file is written asynchronously');
+      fs.writeFile('test.txt', data + "\n [DELETE] " + req.body.name, function (err, data) {
+        if (err) {
+          return console.error(err);
+        }
+        console.log('file is written asynchronously');
+      })
     })
-  })
-  res.send("Sucess")
+    res.send("Sucess")
   }
-  else{
+  else {
     res.send("erreur")
   }
-  })
+})
 
 
-  app.listen(3000, () => {
-    console.log("En attente de requêtes...")
-  })
+app.listen(3000, () => {
+  console.log("En attente de requêtes...")
+})
 
 
