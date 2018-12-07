77059554f500c7e301799b3924c080996daffb96
Elise Marie <elise.marie.lau@gmail.com>
2018-12-07 11:13:17 +0100

    generate jwt method

--- models/User.js
+++ models/User.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+var JWT_SECRET = process.env.JWT_SECRET || 'shhhhh';
 
-var schema = new mongoose.Schema({
+/* 
+Define User schema
+ */
+
+var User = new mongoose.Schema({
     email: {
         type: String,
         required: true
@@ -28,3 +34,32 @@ var schema = new mongoose.Schema({
         default: Date.now(),
     }
 })
+
+/*
+ User methods 
+ */
+
+User.methods.generateAuthToken = function(){
+    return jwt.sign(this._id, JWT_SECRET);
+  };
+
+User.methods.checkPassword = function(password) {
+    
+}
+
+/*
+Static methods
+ */
+
+User.statics.getByToken = function(token, callback){
+    jwt.verify(token, JWT_SECRET, function(err, id){
+      if (err) {
+        return callback(err);
+      }
+      this.findOne({_id: id}, callback);
+    }.bind(this));
+  };
+  
+
+
+//module.exports = mongoose.model('User', User);
